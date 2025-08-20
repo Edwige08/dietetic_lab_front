1068c57e2d3adc83ffda9d00a09af9eb8ae38078
Edwige08 <edwige.saves@gmail.com>
2025-08-20 11:09:34 +0200

    create: component to evaluate undernutrition for Adults

---
+++ src/components/UndernutritionAdult.tsx
@@ -0,0 +1,312 @@
+'use client'
+
+import { useState } from "react";
+import Input from "./Input";
+import ButtonGreen from "./ButtonGreen";
+import InputCheckbox from "./InputCheckbox";
+
+export default function UndernutritionAdult() {
+    const [parameters, setParameters] = useState<WeightHeight>({
+        weight: 0,
+        height: 0,
+        previousWeight: 0,
+        previousWeightDate: "none",
+        albuminemia: 0,
+        sarcopenia: false,
+        firstEtiological: false,
+        secondEtiological: false,
+        thirdEtiological: false,
+    });
+    const [calculDone, setCalculDone] = useState<boolean>(false);
+    const [evaluationResults, setEvaluationResults] = useState<EvaluationResults>({
+        weight: 0,
+        height: 0,
+        imc: 0,
+        previousWeight: 0,
+        previousWeightDate: "none",
+        weightLoss: 0,
+        albuminemia: 0,
+        sarcopenia: false,
+        firstEtiological: false,
+        secondEtiological: false,
+        thirdEtiological: false,
+    });
+    const [isFirstEtiological, setIsFirstEtiological] = useState<boolean>(false)
+    const [isSecondEtiological, setIsSecondEtiological] = useState<boolean>(false)
+    const [isThirdEtiological, setIsThirdEtiological] = useState<boolean>(false)
+    const [isSarcopenia, setIsSarcopenia] = useState<boolean>(false)
+
+    interface WeightHeight {
+        weight: number,
+        height: number,
+        previousWeight: number,
+        previousWeightDate: "none" | "one-month" | "six-month" | "before-disease",
+        albuminemia: number,
+        sarcopenia: boolean,
+        firstEtiological: boolean,
+        secondEtiological: boolean,
+        thirdEtiological: boolean,
+    }
+
+    interface EvaluationResults {
+        weight: number,
+        height: number,
+        imc: number,
+        previousWeight: number,
+        previousWeightDate: "none" | "one-month" | "six-month" | "before-disease",
+        weightLoss: number,
+        albuminemia: number,
+        sarcopenia: boolean,
+        firstEtiological: boolean,
+        secondEtiological: boolean,
+        thirdEtiological: boolean,
+    }
+
+    const textPreviousWeight = (data: string) => {
+        if (data == "none") return "";
+        if (data == "one-month") return "il y a 1 mois ou plus";
+        if (data == "six-month") return "il y a 6 mois ou plus";
+        if (data == "before-disease") return "avant le début de la maladie";
+    }
+
+    const resetForm = () => {
+        setParameters({ weight: 0, height: 0, previousWeight: 0, previousWeightDate: "none", albuminemia: 0, sarcopenia: false, firstEtiological: false, secondEtiological: false, thirdEtiological: false });
+        setCalculDone(false);
+        setIsSarcopenia(false);
+        setIsFirstEtiological(false);
+        setIsSecondEtiological(false);
+        setIsThirdEtiological(false);
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setParameters(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleChangeSarcopenia = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsSarcopenia(!isSarcopenia)
+    }
+
+    const handleChangeFirstEtiological = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsFirstEtiological(!isFirstEtiological)
+    }
+
+    const handleChangeSecondEtiological = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsSecondEtiological(!isSecondEtiological)
+    }
+
+    const handleChangeThirdEtiological = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsThirdEtiological(!isThirdEtiological)
+    }
+
+    const handleChangePreviousWeightDate = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = event.target;
+
+        setParameters(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (parameters.weight > 0 && parameters.height > 0) {
+            const imc = parameters.weight / ((parameters.height / 100) * (parameters.height / 100));
+            setCalculDone(true);
+            setEvaluationResults({ weight: parameters.weight, height: parameters.height, imc: imc, previousWeight: parameters.previousWeight, previousWeightDate: parameters.previousWeightDate, weightLoss: 0, albuminemia: parameters.albuminemia, sarcopenia: isSarcopenia, firstEtiological: isFirstEtiological, secondEtiological: isSecondEtiological, thirdEtiological: isThirdEtiological })
+        }
+
+        if (parameters.weight > 0 && parameters.previousWeight > 0) {
+            const weightLoss = ((parameters.previousWeight - parameters.weight) / parameters.previousWeight * 100).toFixed(1);
+            setEvaluationResults(prev => ({
+                ...prev,
+                weightLoss: parseInt(weightLoss)
+            }))
+        }
+    }
+
+    return (
+        <>
+            <form
+                className="flex flex-col gap-4 p-4 m-3 w-[90%] md:w-[75%] bg-white border border-gray-300 rounded-xl shadow-xl"
+                onSubmit={handleSubmit}
+            >
+                <h2 className="text-xl font-bold text-(--greenColor)">
+                    Evaluation de la dénutrition (≥ 18 ans et &lt; 70 ans)
+                </h2>
+                <h3 className="text-lg font-bold text-(--orangeColor)">
+                    - <span className="underline">Critères phénotypiques</span> :
+                </h3>
+                <ul className="flex flex-col gap-1 px-7 list-disc">
+                    <li>
+
+                        <Input
+                            title="Poids actuel :"
+                            name="weight"
+                            value={parameters.weight}
+                            onChange={handleChange}
+                            unity="kg"
+                        />
+                    </li>
+                    <li>
+                        <Input
+                            title="Taille :"
+                            name="height"
+                            value={parameters.height}
+                            onChange={handleChange}
+                            unity="cm"
+                        />
+                    </li>
+                    <li>
+                        <Input
+                            title="Poids antérieur : "
+                            name="previousWeight"
+                            value={parameters.previousWeight}
+                            onChange={handleChange}
+                            unity="kg"
+                        />
+                    </li>
+                    <li>
+                        <div className="flex flex-col gap-1 justify-center">
+                            <label
+                                htmlFor="previousWeightDate"
+                            >
+                                Ancienneté du poids antérieur :
+                            </label>
+                            <select className="p-2 border border-gray-300 rounded-lg"
+                                onChange={handleChangePreviousWeightDate}
+                                name="previousWeightDate"
+                                value={parameters.previousWeightDate}
+                            >
+                                <option value="none">Choisir une option</option>
+                                <option value="one-month">il y a 1 mois ou plus</option>
+                                <option value="six-month">il y a 6 mois ou plus</option>
+                                <option value="before-disease">habituel avant le début de la maladie</option>
+                            </select>
+                        </div>
+                    </li>
+                    <li>
+                        <Input
+                            title="Albuminémie : "
+                            name="albuminemia"
+                            value={parameters.albuminemia}
+                            onChange={handleChange}
+                            unity="g/L"
+                        />
+                    </li>
+                    <li>
+                        <InputCheckbox
+                            name="sarcopenia"
+                            checked={isSarcopenia}
+                            title="Réduction quantifiée de la masse musculaire et/ou de la fonction musculaire"
+                            onChange={handleChangeSarcopenia}
+                        />
+                    </li>
+                </ul>
+                <h3 className="text-lg font-bold text-(--orangeColor)">
+                    - <span className="underline">Critères étiologiques</span> :
+                </h3>
+                <ul className="flex flex-col gap-1 px-7 list-disc">
+                    <li>
+                        <InputCheckbox
+                            name="firstEtiological"
+                            checked={isFirstEtiological}
+                            title="Réduction de la prise alimentaire ≥ 50 % pendant plus d’1 semaine, ou toute réduction des apports pendant plus de 2 semaines par rapport à la consommation alimentaire habituelle quantifiée ou aux besoins protéino-énergétiques estimés"
+                            onChange={handleChangeFirstEtiological}
+                        />
+                    </li>
+                    <li>
+                        <InputCheckbox
+                            name="secondEtiological"
+                            checked={isSecondEtiological}
+                            title="Absorption réduite (malabsorption/maldigestion)"
+                            onChange={handleChangeSecondEtiological}
+                        />
+                    </li>
+                    <li>
+                        <InputCheckbox
+                            name="thirdEtiological"
+                            checked={isThirdEtiological}
+                            title="Situation d’agression (hypercatabolisme protéique avec ou sans syndrome inflammatoire) : pathologie aiguë ou pathologie chronique évolutive ou pathologie maligne évolutive"
+                            onChange={handleChangeThirdEtiological}
+                        />
+                    </li>
+                </ul>
+                <ButtonGreen
+                    text="Calculer"
+                    type="submit"
+                />
+                {calculDone ?
+                    <button
+                        type="reset"
+                        onClick={resetForm}
+                        className="underline"
+                    >
+                        Reset
+                    </button> : ""
+                }
+            </form>
+            {calculDone ?
+                <div
+                    className="flex flex-col gap-4 p-4 m-3 w-[90%] md:w-[75%] bg-(--orangeLightColor) border border-gray-300 rounded-xl shadow-xl"
+                >
+                    <h2 className="text-lg font-bold">
+                        ✅ Résultat :
+                    </h2>
+                    <p>
+                        Pour un poids de <span className="font-bold">{evaluationResults.weight} kg</span> et une taille de <span className="font-bold">{evaluationResults.height} cm</span>, on obtient un IMC de <span className="font-bold">{evaluationResults.imc.toFixed(2)} kg/m²</span>.
+                    </p>
+                    {evaluationResults.previousWeight > 0 && evaluationResults.weight > 0 ?
+                        <p>
+                            Avec un poids antérieur {evaluationResults.previousWeightDate != 'none' ? `(${textPreviousWeight(evaluationResults.previousWeightDate)})` : ""} de <span className="font-bold">{evaluationResults.previousWeight} kg</span>, la perte de poids est estimée à <span className="font-bold">{evaluationResults.weightLoss} %</span>.
+                        </p> : ""
+                    }
+                    <div>
+                        <p className="underline">
+                            Critère(s) phénotypique(s) :
+                        </p>
+                        {(((evaluationResults.weightLoss >= 5) && (evaluationResults.previousWeightDate === "one-month")) || ((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "six-month")) || ((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "before-disease")) || (evaluationResults.imc < 22) || (evaluationResults.sarcopenia)) ?
+                            <ul className="pl-5 list-disc">
+                                {((evaluationResults.weightLoss >= 5) && (evaluationResults.previousWeightDate === "one-month")) ? <li>Perte de poids de {evaluationResults.weightLoss} % en 1 mois ou plus</li> : ""}
+                                {((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "six-month")) ? <li>Perte de poids de {evaluationResults.weightLoss} % en 6 mois ou plus</li> : ""}
+                                {((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "before-disease")) ? <li>Perte de poids de {evaluationResults.weightLoss} % depuis le début de la maladie</li> : ""}
+                                {(evaluationResults.imc < 18.5) ? <li>IMC = {evaluationResults.imc.toFixed(2)} kg/m²</li> : ""}
+                                {evaluationResults.sarcopenia ? <li>Réduction quantifiée de la masse musculaire et/ou de la fonction musculaire</li> : ""}
+                            </ul>
+                            : "Aucun"}
+                    </div>
+                    <div>
+                        <p className="underline">
+                            Critère(s) étiologique(s) :
+                        </p>
+                        {(evaluationResults.firstEtiological || evaluationResults.secondEtiological || evaluationResults.thirdEtiological) ?
+                            <ul className="pl-5 list-disc">
+                                {(evaluationResults.firstEtiological) ? <li>Réduction de la prise alimentaire</li> : ""}
+                                {(evaluationResults.secondEtiological) ? <li>Absorption réduite</li> : ""}
+                                {(evaluationResults.thirdEtiological) ? <li>Situation d'agression</li> : ""}
+                            </ul>
+                            : "Aucun"}
+                    </div>
+                    {(((evaluationResults.weightLoss >= 5) && (evaluationResults.previousWeightDate === "one-month")) || ((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "six-month")) || ((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "before-disease")) || (evaluationResults.imc < 18.5) || (evaluationResults.sarcopenia)) && (evaluationResults.firstEtiological || evaluationResults.secondEtiological || evaluationResults.thirdEtiological) ?
+                        <div>
+                            <>
+                                <p>En présence d'au moins un critère phénotypique et un critère étiologique, le diagnostic de dénutrition est confirmé.</p>
+
+                                {((evaluationResults.imc <= 17) || ((evaluationResults.weightLoss >= 10) && (evaluationResults.previousWeightDate === "one-month")) || ((evaluationResults.weightLoss >= 15) && (evaluationResults.previousWeightDate === "six-month")) || ((evaluationResults.weightLoss >= 15) && (evaluationResults.previousWeightDate === "before-disease")) || (evaluationResults.albuminemia <= 30)) ?
+                                    <p>Il s'agit ici d'une dénutrition sévère.</p>
+                                    : <p>Il s'agit ici d'une dénutrition modérée.</p>}
+
+                            </>
+                        </div>
+                        : <p>En l'absence d'au moins un critère phénotypique et un critère étiologique, on ne peut pas poser le diagnostique de dénutrition. En ambulatoire, le patient est à réévaluer à chaque consultation. En cas d'hospitalisation, réévaluation une fois par semaine (en MCO) ou toutes les 2 semaines (en SSR).</p>}
+                </div>
+                : ""
+            }
+        </>
+    )
+}
