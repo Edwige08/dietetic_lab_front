cd1a8c34bc141d220eaf23e54cfc5950939b1d86
Edwige08 <edwige.saves@gmail.com>
2025-08-21 15:16:09 +0200

    update: button green with daisyUI

--- src/components/ButtonGreen.tsx
+++ src/components/ButtonGreen.tsx
@@ -1,10 +1,20 @@
-export default function ButtonGreen(props: { type?: "submit" | "button", text: string }) {
+import { LucideIcon } from "lucide-react";
+
+interface ButtonGreenProps {
+    type?: "submit" | "button";
+    text: string;
+    lucide?: LucideIcon;
+}
+
+export default function ButtonGreen({
+    type, text, lucide: Icon }: ButtonGreenProps) {
     return (
         <button
-            type={props.type}
-            className="p-2 rounded-lg font-bold bg-(--greenColor) shadow-lg hover:bg-(--greenSecondColor) cursor-pointer"
-            >
-            {props.text}
+            type={type}
+            className="btn text-lg rounded-lg bg-(--greenColor) shadow-lg hover:bg-(--greenSecondColor) cursor-pointer"
+        >
+            <p>{Icon && <Icon />}</p>
+            <p>{text}</p>
         </button>
     )
 }
