3e9349133b0adab0f3c7187834dda938e12d72d7
Edwige08 <edwige.saves@gmail.com>
2025-08-21 15:17:54 +0200

    refacto: CardSection component with daisyUI

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,43 +1,6 @@
-// export default function CardSection(props: { bgColor: string, title: string, description: string }) {
-//     return (
-//         <article className="flex flex-row gap-4 p-4 m-3 border border-gray-300 rounded-xl shadow-xl bg-white hover:translate-1 cursor-pointer duration-120 ease-in-out">
-//             <div className={`p-3 size-fit rounded-lg text-3xl ${props.bgColor}`}>
-//                 ⚖
-//             </div>
-//             <div className="flex flex-col gap-1">
-//                 <h2 className="font-bold text-xl">
-//                     {props.title}
-//                 </h2>
-//                 <p className="mb-2 text-gray-700">
-//                     {props.description}
-//                 </p>
-//                 <p className="">
-//                     Utiliser l'outil
-//                 </p>
-//             </div>
-//         </article>
-//     )
-// }
-
 export default function CardSection(props: { bgColor: string, emoji: string, title: string, description: string }) {
     return (
         <>
-            {/* <article className="flex flex-row gap-4 p-4 m-3 border border-gray-300 rounded-xl shadow-xl bg-white hover:translate-1 cursor-pointer duration-120 ease-in-out">
-                <div className={`p-3 size-fit rounded-lg text-3xl ${props.bgColor}`}>
-                    {props.emoji}
-                </div>
-                <div className="flex flex-col gap-1">
-                    <h2 className="font-bold text-xl">
-                        {props.title}
-                    </h2>
-                    <p className="mb-2 text-gray-700">
-                        {props.description}
-                    </p>
-                    <p className="">
-                        Utiliser l'outil
-                    </p>
-                </div>
-            </article> */}
             <div className="card card-side bg-white shadow-md mx-3 my-2">
                 <figure className={`p-3 rounded-lg text-3xl w-20 ${props.bgColor}`}>
                     {props.emoji}
