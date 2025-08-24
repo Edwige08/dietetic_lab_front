4ea35074d142f0f4a4e6efa6cb14d64399909965
Edwige08 <edwige.saves@gmail.com>
2025-08-24 18:01:11 +0200

    add: links to the pages on the Dock

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,29 +1,38 @@
+'use client'
+
 import { Calculator, House, Popcorn, UserRound } from "lucide-react"
+import { useRouter, usePathname } from "next/navigation";
 
 export default function Dock() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleNavigation = (path: string) => {
+        router.push(path);
+    }
     return (
         <div className="dock dock-xl bg-white">
-            <button className="dock-active">
+            <button className={pathname === "/" ? "dock-active" : ""} onClick={() => handleNavigation('/')}>
                 <House />
                 <span className="dock-label">Home</span>
             </button>
 
-            <button>
+            <button className={pathname === "/imc" ? "dock-active" : ""} onClick={() => handleNavigation('/imc')}>
                 <Calculator />
                 <span className="dock-label">IMC</span>
             </button>
             
-            <button>
+            <button onClick={() => handleNavigation('/')}>
                 <Popcorn />
                 <span className="dock-label">Ingesta</span>
             </button>
 
-            <button>
+            <button className={pathname === "/dej" ? "dock-active" : ""} onClick={() => handleNavigation('/dej')}>
                 <Calculator />
                 <span className="dock-label">DEJ</span>
             </button>
 
-            <button>
+            <button onClick={() => handleNavigation('/')}>
                 <UserRound />
                 <span className="dock-label">Mon compte</span>
             </button>
