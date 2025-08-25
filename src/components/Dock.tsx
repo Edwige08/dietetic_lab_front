e3bc866b9ca7046cf4ea643287fd871c0d90c99e
Edwige08 <edwige.saves@gmail.com>
2025-08-25 15:59:30 +0200

    update: dock adding a Drawer

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Calculator, House, Popcorn, UserRound } from "lucide-react"
+import { House, Popcorn, UserRound } from "lucide-react"
+import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
 
 export default function Dock() {
@@ -12,30 +13,56 @@ export default function Dock() {
     }
     return (
         <div className="dock dock-xl bg-white">
-            <button className={pathname === "/imc" ? "dock-active" : ""} onClick={() => handleNavigation('/imc')}>
-                <Calculator />
-                <span className="dock-label">IMC</span>
-            </button>
-            
-            <button onClick={() => handleNavigation('/')}>
-                <Popcorn />
-                <span className="dock-label">Ingesta</span>
-            </button>
-            
-            <button className={pathname === "/" ? "dock-active" : ""} onClick={() => handleNavigation('/')}>
-                <House />
-                <span className="dock-label">Home</span>
-            </button>
-
-            <button className={pathname === "/dej" ? "dock-active" : ""} onClick={() => handleNavigation('/dej')}>
-                <Calculator />
-                <span className="dock-label">DEJ</span>
-            </button>
 
             <button onClick={() => handleNavigation('/')}>
                 <UserRound />
                 <span className="dock-label">Mon compte</span>
             </button>
+
+            <div className="drawer">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    <label htmlFor="my-drawer" className="drawer-button flex flex-col justify-center items-center">
+                        <Popcorn />
+                        <span className="dock-label">Outils diet</span>
+                    </label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <div className="menu bg-base-200 text-base-content h-full w-60 p-4 justify-between bg-white text-black">
+                        <div>
+                            <p className="text-xl underline mb-3">Les outils disponibles :</p>
+                            <ul className="flex flex-col gap-2">
+                                <li className="flex justify-center border bg-(--orangeLightColor) rounded-xl h-12 shadow-md"><Link href="/imc" className="text-lg">🥝 Calcul IMC</Link></li>
+                                <li className="flex justify-center border bg-(--greenLightColor) rounded-xl h-12 shadow-md"><Link href="/dej" className="text-lg">🍌 Calcul DEJ</Link></li>
+                                <li className="flex justify-center border bg-(--redLightColor) rounded-xl h-12 shadow-md"><Link href="/undernutrition" className="text-lg">🥥 Eval dénutrition</Link></li>
+                                <li className="flex justify-center border bg-(--yellowLightColor) rounded-xl h-12 shadow-md"><Link href="/sri" className="text-lg">🍓 Evaluation SRI</Link></li>
+                                <li className="flex justify-center border bg-(--grayMediumColor) rounded-xl h-12 shadow-md"><Link href="/ingesta" className="text-lg">🍇 Calcul Ingesta</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="text-end">
+                            <li><Link href="/about" className="text-md">A propos</Link></li>
+                            <li><Link href="/" className="text-md">Vos retours</Link></li>
+                            <li><Link href="/" className="text-md">Mentions légales</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            {/* <button onClick={() => handleNavigation('/')}>
+                <Popcorn />
+                <span className="dock-label">Outils diet</span>
+            </button> */}
+
+            <button className={pathname === "/" ? "dock-active" : ""} onClick={() => handleNavigation('/')}>
+                <House />
+                <span className="dock-label">Home</span>
+            </button>
+
+            {/* <button className={pathname === "/dej" ? "dock-active" : ""} onClick={() => handleNavigation('/dej')}>
+                <Calculator />
+                <span className="dock-label">DEJ</span>
+            </button> */}
+
         </div>
     )
 }
