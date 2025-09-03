91fb4ef15ce1ba22f11db561796ded8e99e09888
Edwige08 <edwige.saves@gmail.com>
2025-09-03 12:42:09 +0200

    add: button to signin on navbar (large screens)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserRound } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +8,7 @@ export default function Navbar() {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                     </div>
                     <ul
                         tabIndex={0}
@@ -40,7 +41,12 @@ export default function Navbar() {
                     </p>
                 </Link>
             </div>
-            <div className="navbar-end"></div>
+            <div className="navbar-end">
+                <Link href="/signin" className="flex flex-row gap-2 text-lg hover:underline">
+                <UserRound />
+                <p>Mon compte</p>
+                </Link>
+            </div>
         </div>
     )
 }
