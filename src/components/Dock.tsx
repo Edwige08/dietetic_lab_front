ea6708bdb8f1dc629f69b02863c03102f0b1d076
Edwige08 <edwige.saves@gmail.com>
2025-09-10 11:33:09 +0200

    update: components Dock and Return

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -19,47 +19,48 @@ export default function Dock() {
     }
 
     return (
-        <div className="dock dock-xl bg-white text-black">
+        <div className="dock dock-xl bg-white shadow-xl border-(--grayLightColor)">
 
             <button className={`w-[20%] ${pathname === "/about" || pathname === "/faq" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} >
                 <div className="dropdown dropdown-top">
-                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><LibraryBig /> Infos</div>
-                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-30 p-2 shadow-sm">
-                        <li><Link href="/about">A propos</Link></li>
-                        <li><Link href="/faq">FAQ</Link></li>
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium text-black shadow-none"><LibraryBig /> Infos</div>
+                    <ul tabIndex={0} className="dropdown-content menu active:bg-white bg-white opacity-100 rounded-3xl z-1 w-30 p-1 gap-1 border border-(--grayLightColor) mb-3">
+                        <li className=" bg-(--grayLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="flex flex-col justify-center items-center active:bg-white h-13" href="/about">A propos</Link></li>
+                        <li className=" bg-(--grayLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="flex flex-col justify-center items-center active:bg-white h-13" href="/faq">FAQ</Link></li>
                     </ul>
                 </div>
             </button>
 
             <button className={`w-[20%] ${pathname === "/imc" || pathname === "/dej" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`}>
-                <div className="dropdown dropdown-top">
-                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Calculator /> Calculs</div>
-                    <ul tabIndex={0} className="dropdown-content menu bg-white rounded-box z-1 w-30 p-2 shadow-sm">
-                        <li><Link href="/imc">IMC</Link></li>
-                        <li><Link href="/dej">DEJ</Link></li>
+                <div className="dropdown dropdown-top dropdown-center">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium text-black shadow-none"><Calculator /> Calculs</div>
+                    <ul tabIndex={0} className="dropdown-content menu active:bg-white bg-white opacity-100 rounded-3xl z-1 w-30 p-1 gap-1 border border-(--grayLightColor) mb-3">
+                        <li className=" bg-(--orangeLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="text-center active:bg-white" href="/imc">🥝 <br /> IMC</Link></li>
+                        <li className=" bg-(--greenLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="text-center active:bg-white" href="/dej">🍌 <br /> DEJ</Link></li>
                     </ul>
                 </div>
             </button>
 
-            <button className={`w-[20%] ${pathname === "/" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} onClick={() => handleNavigationProfile()}>
+            <button className={`w-[20%] ${pathname === "/" || pathname === "/personnalProfile" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} onClick={() => handleNavigationProfile()}>
                 <div className="dropdown dropdown-top">
-                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><House /> Home</div>
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium text-black shadow-none"><House /> Home</div>
                 </div>
             </button>
 
             <button className={`w-[20%] ${pathname === "/undernutrition" || pathname === "/sri" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`}>
-                <div className="dropdown dropdown-top">
-                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Search /> Evaluations</div>
-                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 opacity-100 rounded-box z-1 w-30 p-2 shadow-sm">
-                        <li><Link href="/undernutrition">Dénutrition</Link></li>
-                        <li><Link href="/sri">SRI</Link></li>
+                <div className="dropdown dropdown-top dropdown-center">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium text-black shadow-none"><Search /> Evaluations</div>
+                    <ul tabIndex={0} className="dropdown-content menu active:bg-white bg-white opacity-100 rounded-3xl z-1 w-30 p-1 gap-1 border border-(--grayLightColor) mb-3">
+                        <li className=" bg-(--redLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="text-center active:bg-white" href="/undernutrition">🥥 <br /> Dénutrition</Link></li>
+                        <li className="bg-(--yellowLightColor) rounded-3xl flex flex-col justify-center items-center active:bg-white border border-(--grayLightColor)"><Link className="text-center active:bg-white" href="/sri">🍓 <br /> SRI</Link></li>
+                        {/* <li className="text-2xl">^</li> */}
                     </ul>
                 </div>
             </button>
 
             <button className={`w-[20%] ${pathname === "/ingesta" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} onClick={() => handleNavigation('/ingesta')}>
                 <div className="dropdown dropdown-top">
-                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Utensils /> Ingesta</div>
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium text-black shadow-none"><Utensils /> Ingesta</div>
                 </div>
             </button>
         </div>
--- src/components/Return.tsx
+++ src/components/Return.tsx
@@ -17,11 +17,15 @@ export default function Return() {
     }
 
     return (
-        <div className="flex flex-row justify-between items-center px-5 h-15">
+        <div className="flex flex-row justify-between items-center px-5 h-15 shadow-lg">
+            {pathname === '/personnalProfile' ?
+            <div></div>
+            :
             <button onClick={() => handleNavigationProfile()} className="flex flex-row items-center justify-start gap-2 w-60">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path className="fill-(--greenSecondColor)" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path></svg>
                     <p className="text-(--greenSecondColor) hover:underline">Accueil</p>
             </button>
+            }
             {isAuthenticated && user &&
                 <Link href="/personnalProfile" className="flex flex-row gap-2 justify-center items-center text-(--greenSecondColor) hover:underline">
                     <UserRoundCheck />
