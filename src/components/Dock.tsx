e6507b31306b35906f5bbfc9dfffa29e2e2fe5f8
Edwige08 <edwige.saves@gmail.com>
2025-09-10 08:01:30 +0200

    update: links in Dock

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { useUser } from "@/contexts/UserContext";
-import { House, Popcorn, UserRound } from "lucide-react"
+import { Calculator, House, LibraryBig, Popcorn, Search, UserRound, Utensils } from "lucide-react"
 import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
 
 export default function Dock() {
     const router = useRouter();
     const pathname = usePathname();
-    const { isAuthenticated } = useUser();
+    const { user, isAuthenticated } = useUser();
 
     const handleNavigationProfile = () => {
         isAuthenticated ? router.push("/personnalProfile") : router.push('/signin');
@@ -20,6 +20,54 @@ export default function Dock() {
 
     return (
         <div className="dock dock-xl bg-white">
+
+            <button className={`${pathname === "/about" || pathname === "/faq" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} >
+                <div className="dropdown dropdown-top">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><LibraryBig /> Infos</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-30 p-2 shadow-sm">
+                        <li><Link href="/about">A propos</Link></li>
+                        <li><Link href="/faq">FAQ</Link></li>
+                    </ul>
+                </div>
+            </button>
+
+            <button className={`${pathname === "/imc" || pathname === "/dej" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`}>
+                <div className="dropdown dropdown-top">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Calculator /> Calculs</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-white rounded-box z-1 w-30 p-2 shadow-sm">
+                        <li><Link href="/imc">IMC</Link></li>
+                        <li><Link href="/dej">DEJ</Link></li>
+                    </ul>
+                </div>
+            </button>
+
+            <button className={`${pathname === "/" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} onClick={() => handleNavigation('/')}>
+                <div className="dropdown dropdown-top">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><House /> Home</div>
+                </div>
+            </button>
+
+            <button className={`${pathname === "/undernutrition" || pathname === "/sri" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`}>
+                <div className="dropdown dropdown-top">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Search /> Evaluations</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 opacity-100 rounded-box z-1 w-30 p-2 shadow-sm">
+                        <li><Link href="/undernutrition">Dénutrition</Link></li>
+                        <li><Link href="/sri">SRI</Link></li>
+                    </ul>
+                </div>
+            </button>
+
+            <button className={`${pathname === "/ingesta" ? "border-none bg-(--grayLightColor) rounded-4xl" : ""}`} onClick={() => handleNavigation('/ingesta')}>
+                <div className="dropdown dropdown-top">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col h-full border-0 bg-transparent font-medium"><Utensils /> Ingesta</div>
+                </div>
+            </button>
+        </div>
+    )
+}
+
+
+{/* <div className="dock dock-xl bg-white">
 
             <button className={pathname === "/signin" ? "dock-active" : ""} onClick={() => handleNavigationProfile()}>
                 <UserRound />
@@ -61,6 +109,4 @@ export default function Dock() {
                 <span className="dock-label">Home</span>
             </button>
 
-        </div>
-    )
-}
+        </div> */}
