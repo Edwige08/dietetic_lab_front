02c1cf61eb5571a2423dcf230490530359a04c7d
Edwige08 <edwige.saves@gmail.com>
2025-09-17 11:28:55 +0200

    update: faq content

--- src/app/informations/page.tsx
+++ src/app/informations/page.tsx
@@ -43,24 +43,17 @@ export default function Informations() {
                             text="Foire aux Questions"
                         />
                         <div className="flex flex-col p-4 m-3 w-[90%] md:w-[75%] bg-white border border-gray-300 rounded-xl shadow-xl text-justify">
-                            <TitleTwo text="Je ne comprends pas ce qui est écrit dans cette FAQ. Pourquoi&nbsp;?" />
+                            <TitleTwo text="Qu'est-ce que Dietetic Lab&nbsp;?" />
                             <p>
-                                Ne vous inquiétez pas, c&apos;est normal. Cette page est encore en cours de construction. Le texte que vous lisez est du <span className="italic">lorem ipsum</span>. C&apos;est une suite de mots sans signification que j&apos;utilise pour la mise en page. Il sera bientôt transformé en une vraie FAQ.
+                                Dietetic Lab est un outil en ligne spécialement conçu pour les professionnels de la nutrition et de la diététique qui propose une suite de calculateurs et d'outils d'évaluation nutritionnelle pour faciliter le travail.
                             </p>
-                            <br />
-                            <TitleTwo text="Ma question n°2 de la FAQ&nbsp;?" />
+                            <TitleTwo text="À qui s'adresse cet outil&nbsp;?" />
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra mauris a dictum scelerisque. Aenean id suscipit nunc. Donec convallis nunc nec ex suscipit vulputate. Ut tempor magna vel imperdiet scelerisque. Nam pulvinar mauris vel accumsan tristique. Quisque vehicula tortor risus, et venenatis nulla condimentum id. Donec vitae est lacus. Nunc pretium purus ac velit maximus gravida.
+                                Dietetic Lab s'adresse principalement aux diététicien.ne.s, nutritionnistes et étudiants en nutrition.
                             </p>
-                            <br />
-                            <TitleTwo text="Ma question n°3 de la FAQ&nbsp;?" />
+                            <TitleTwo text="Dietetic Lab est-il gratuit&nbsp;?" />
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra mauris a dictum scelerisque. Aenean id suscipit nunc. Donec convallis nunc nec ex suscipit vulputate. Ut tempor magna vel imperdiet scelerisque. Nam pulvinar mauris vel accumsan tristique. Quisque vehicula tortor risus, et venenatis nulla condimentum id. Donec vitae est lacus. Nunc pretium purus ac velit maximus gravida.
-                            </p>
-                            <br />
-                            <TitleTwo text="Ma question n°4 de la FAQ&nbsp;?" />
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra mauris a dictum scelerisque. Aenean id suscipit nunc. Donec convallis nunc nec ex suscipit vulputate. Ut tempor magna vel imperdiet scelerisque. Nam pulvinar mauris vel accumsan tristique. Quisque vehicula tortor risus, et venenatis nulla condimentum id. Donec vitae est lacus. Nunc pretium purus ac velit maximus gravida.
+                                Oui, Dietetic Lab est un ensemble d'outils gratuits.
                             </p>
                         </div>
                     </div>
