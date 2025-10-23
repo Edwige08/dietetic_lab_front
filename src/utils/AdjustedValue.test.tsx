8c6119fbcf8eb0909e941116b2ff9192de657037
Edwige08 <edwige.saves@gmail.com>
2025-10-23 12:19:25 +0200

    update: adjustedValue and its tests to improve code coverage

--- src/utils/AdjustedValue.test.tsx
+++ src/utils/AdjustedValue.test.tsx
@@ -1,26 +1,36 @@
 import { expect, test } from 'vitest';
 import { adjustedValue } from './AdjustedValue';
 
-
 test('adjustedValue("0", 100) is 0', () => {
     expect(adjustedValue("0", 100,)).toBe(0);
 })
 test('adjustedValue("10", 100) is 10', () => {
     expect(adjustedValue("10", 100,)).toBe(10);
 })
-
 test('adjustedValue("10", 200) is 20', () => {
     expect(adjustedValue("10", 200,)).toBe(20);
 })
-
 test('adjustedValue("10,1", 200) is 20.2', () => {
     expect(adjustedValue("10,1", 200,)).toBe(20.2);
 })
-
 test('adjustedValue("10.1", 200) is 20.2', () => {
     expect(adjustedValue("10.1", 200,)).toBe(20.2);
 })
-
 test('adjustedValue("-", 200) is 0', () => {
     expect(adjustedValue("-", 200,)).toBe(0);
+})
+test('adjustedValue(0, "100") is 0', () => {
+    expect(adjustedValue(0, 100)).toBe(0);
+})
+test('adjustedValue(0, 100) is 0', () => {
+    expect(adjustedValue(0, 100)).toBe(0);
+})
+test('adjustedValue(10, 100) is 10', () => {
+    expect(adjustedValue(10, 100)).toBe(10);
+})
+test('adjustedValue(10, 200) is 20', () => {
+    expect(adjustedValue(10, 200)).toBe(20);
+})
+test('adjustedValue(10.1, 200) is 20.2', () => {
+    expect(adjustedValue(10.1, 200)).toBe(20.2);
 })
